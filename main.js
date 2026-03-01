c30a324ef8f43b9a6d89b6cd151d849cb029ed90
AI researcher <57758974+liam274@users.noreply.github.com>
2026-03-01 10:43:32 +0800

    evaulateExpression Update, major bug fixed.

--- main.js
+++ main.js
@@ -822,16 +822,28 @@ class render {
                 } else varName.push(char);
             }
             let name = varName.join("").trim();
-            if (extraScope[name]) {
-                if (typeof extraScope[name] === "function") result = extraScope[name](result);
+            if (first) {
+                if (extraScope[name]) {
+                    result = extraScope[name];
+                }
                 else {
-                    error(`[ezy.js] CRITICAL ERROR: Pharsing Error: Error when pharsing, expected filter as function, found ${typeof extraScope[name]}, not found, in ${traceback}`);
+                    error(`[ezy.js] CRITICAL ERROR: Pharsing Error: Error when trying to access variable ${name}, not found, in ${traceback}`);
                     return this.set(5);
                 }
-            }
-            else {
-                error(`[ezy.js] CRITICAL ERROR: Pharsing Error: Error when trying to access variable ${name}, not found, in ${traceback}`);
-                return this.set(5);
+                first = false;
+            } else {
+                name = name.split(":");
+                if (extraScope[name[0]]) {
+                    if (typeof extraScope[name[0]] === "function") result = extraScope[name[0]](result, ...name.filter((_, i) => i));
+                    else {
+                        error(`[ezy.js] CRITICAL ERROR: Pharsing Error: Error when pharsing, expected filter as function, found ${typeof extraScope[name[0]]}, not found, in ${traceback}`);
+                        return this.set(5);
+                    }
+                }
+                else {
+                    error(`[ezy.js] CRITICAL ERROR: Pharsing Error: Error when trying to access variable ${name}, not found, in ${traceback}`);
+                    return this.set(5);
+                }
             }
             return result;
         }
