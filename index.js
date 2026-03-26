a83547676283cd887b5855819678c727c64680ed
AI researcher <57758974+liam274@users.noreply.github.com>
2026-03-26 08:29:23 +0800

    modified:   index.js

--- index.js
+++ index.js
@@ -243,7 +243,7 @@ const pageData = {
                     text: "Normal Q&A",
                     events: {
                         onclick: {
-                            listener: [() => ezy.Ezy.navigate("q&a.html")][0]
+                            listener: [() => ezy.Ezy.navigate("q&a.html")]
                         }
                     },
                     type: ["bg-clr-#e0e0e0", "color-#2E7D32"],
